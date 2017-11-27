92cd4b207016137a9ad6d569e72d70839a35bce2
shenli <shenli@daojia.com>
2017-11-27 16:50:41 +0800

    恢复 Shell读取 args
    Change-Id: Iae27738564359b6ecee43e6629a66e42fa738da3

--- mydog-shell/src/main/java/org/huangpu/mydog/shell/ShellTools.java
+++ mydog-shell/src/main/java/org/huangpu/mydog/shell/ShellTools.java
@@ -12,17 +12,18 @@ import java.io.IOException;
 public class ShellTools {
 
     public static void main(String[] args) throws IOException {
-       /* if (args == null || args.length < 1) {
+       if (args == null || args.length < 1) {
 
             System.err.println("please input the metadata json path. [java -jar mydog-shell-1.0-SNAPSHOT.jar /xxx/xxx.json]");
             System.exit(1);
-        }*/
+        }
 //        args = new String[]{
 //                "/Users/shenli/knowledge/spring/github/MyDog/mydog-shell/demo.json"
 //        };
 
         String metadataJson = "";
-        File metaDataFile = new File("G:\\project\\github\\MyDog\\mydog-shell\\demo.json");
+//        File metaDataFile = new File("G:\\project\\github\\MyDog\\mydog-shell\\demo.json");
+        File metaDataFile = new File(args[0]);
         if (!metaDataFile.exists()) {
             System.err.println("the config file [" + metadataJson + "] is not exist.");
             System.exit(1);
